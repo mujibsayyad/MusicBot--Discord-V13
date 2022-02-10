11b8af9aebfaa843cd19717fff24ab87a04bb906
TheMujib <mujibsayyad97@gmail.com>
2022-02-10 21:34:22 +0530

    Added Slash Commands

--- app.js
+++ app.js
@@ -1,183 +1,96 @@
-const { Client, MessageEmbed } = require('discord.js');
-const Distube = require('distube').default;
+const fs = require('fs');
+const { Client, Collection } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const config = require('./config');
+
 const client = new Client({
   intents: 641,
 });
 
-const distube = new Distube(client, {
-  emitNewSongOnly: false,
-  searchSongs: 0,
-});
+const commands = [];
+client.commands = new Collection();
+
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'));
+
+// Place your client and guild ids here
+const clientId = config.client_id;
+const guildId = config.guild_id;
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  client.commands.set(command.data.name, command);
+  commands.push(command.data.toJSON());
+  console.log(`Loaded ${file}`);
+}
+
+const rest = new REST({ version: '9' }).setToken(config.token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: commands,
+    });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
 client.on('ready', () => {
-  console.log('Music is playing ...');
-  client.user.setActivity('Your Songs', { type: 'PLAYING' });
+  console.log(`${client.user.tag} Music is playing ...`);
+
+  client.user.setActivity(`WATCHING ${client.guilds.cache.size} SERVERS`);
 });
 
-//// distube Events
+// ? *************************************************************************************
 
-// Queue status
-const status = (queue) =>
-  `Volume: \`${queue.volume}%\` | Filter: \`${
-    queue.filters.join(', ') || 'Off'
-  }\` | Loop: \`${
-    queue.repeatMode
-      ? queue.repeatMode === 2
-        ? 'All Queue'
-        : 'This Song'
-      : 'Off'
-  }\` | Autoplay: \`${(queue.autoplay = 'On')}\``;
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-// DisTube event listeners, more in the documentation page
-distube
-  .on('playSong', (queue, song) => {
-    let playEmbed = new MessageEmbed()
-      .setColor('#FFA400')
-      .setTitle(`🎵 Playing `)
-      .setThumbnail(song.thumbnail)
-      .setDescription(`[${song.name}](${song.url})`)
-      .addField('Requested By', `${song.user}`, true)
-      .addField('Duration', `${song.formattedDuration.toString()}`, true)
-      .setFooter(status(queue), song.user.displayAvatarURL({ dynamic: true }));
+  const command = client.commands.get(interaction.commandName);
 
-    queue.textChannel.send({ embeds: [playEmbed] });
-  })
-  .on('addSong', (queue, song) => {
-    let playEmbed = new MessageEmbed()
-      .setColor('#FFA400')
-      .setTitle(`🎵 Added to Queue`)
-      .setThumbnail(song.thumbnail)
-      .setDescription(`[${song.name}](${song.url})`)
-      .addField('Requested By', `${song.user}`, true)
-      .addField('Duration', `${song.formattedDuration.toString()}`, true)
-      .setFooter(`Hey`, song.user.displayAvatarURL({ dynamic: true }));
+  if (!command) return;
+  // const string = interaction.options.getString('input');
+  // console.log('name is' + string);
+  // console.log(command.data.name);
 
-    queue.textChannel.send({ embeds: [playEmbed] });
-  })
-  .on('addList', (queue, playlist) =>
-    queue.textChannel.send(
-      `Added \`${playlist.name}\` playlist (${
-        playlist.songs.length
-      } songs) to queue\n${status(queue)}`
-    )
-  )
-  .on('error', (channel, error) =>
-    channel.send('An error encountered: ' + error)
-  );
-// *************************************************************************************
-
-client.on('messageCreate', async (message) => {
-  if (
-    !message.guild ||
-    message.author.bot ||
-    !message.content.startsWith(config.prefix)
-  )
-    return;
-
-  let args = message.content.slice(config.prefix.length).trim().split(' ');
-  let cmd = args.shift()?.toLowerCase();
-
-  if (cmd === 'ping') {
-    message.channel.send(`>>> Ping :- \`${client.ws.ping}\``);
-  } else if (cmd === 'play') {
-    let search = args.join(' ');
-    let channel = message.member.voice.channel;
-
-    if (!channel) {
-      message.react('🙄');
-      return message.reply({
-        embeds: [
-          new MessageEmbed()
-            .setColor('#FFA400')
-            .setDescription(`>>> Please join a voice channel`)
-            .setFooter(
-              `${message.author.username}`,
-              message.author.displayAvatarURL({ dynamic: true })
-            ),
-        ],
-      });
-    }
-
-    if (!search) {
-      return message.reply({
-        embeds: [
-          new MessageEmbed()
-            .setColor('#FFA400')
-            .setDescription(`>>> Please provide song name or link`)
-            .setFooter(
-              `${message.author.username}`,
-              message.author.displayAvatarURL({ dynamic: true })
-            ),
-        ],
-      });
-    } else {
-      message.react('🥰');
-    }
-
-    distube.play(message, search);
-  } else if (cmd === 'skip') {
-    let queue = distube.getQueue(message.guildId);
-
-    if (!message.guild.me.voice.channel) {
-      return message.reply(`>>> Nothing Playing 😶`);
-    }
-    message.reply(`>>> Skipped`) && queue.skip();
-  }
-
-  if (cmd === 'stop') {
-    distube.stop(message);
-    message.channel.send('Stopped the music!');
-  }
-
-  if (cmd === 'resume') distube.resume(message);
-
-  if (cmd === 'pause') distube.pause(message);
-
-  if (cmd === 'help') {
-    return message.reply({
-      embeds: [
-        new MessageEmbed()
-          .setColor('RANDOM')
-          .setTitle('**Commands**')
-          .setDescription(`**>>> Bot prefix : . **`)
-          .addField(' play : ', ' Plays song ', true)
-          .addField(' skip : ', ' Skip playing song ', true)
-          .addField(' queue : ', ' check all requested songs ', true)
-          .addField(' help : ', ' all commands ', true)
-          .addField(' stop : ', ' stops playing ', true)
-          .addField(' resume :', ' resumes playing ', true)
-          .addField(' pause : ', ' Pauses playing ', true)
-          .setFooter(
-            `${message.author.username}`,
-            message.author.displayAvatarURL({ dynamic: true })
-          ),
-      ],
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.log(error);
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
     });
   }
+});
 
-  if (cmd === 'queue') {
-    const queue = distube.getQueue(message.guildId);
+// ? *************************************************************************************
 
-    // let playEmbed = new MessageEmbed().setColor('#FFA400');
+client.on('messageCreate', async (message) => {
+  if (message.author.bot || !message.guild) return;
 
-    if (!queue) {
-      message.channel.send('Nothing playing right now!');
-    } else {
-      message.channel.send(
-        `Current queue:\n${queue.songs
-          .map(
-            (song, id) =>
-              `**${id ? id : 'Playing'}**. ${song.name} - \`${
-                song.formattedDuration
-              }\``
-          )
-          .slice(0, 10)
-          .join('\n')}`
-      );
-    }
+  const prefix = config.prefix;
+  if (!message.content.startsWith(prefix)) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  const cmd = client.commands.get(command);
+  if (!cmd) return;
+
+  if (cmd.inVoiceChannel && !message.member.voice.channel) {
+    return message.channel.send(`You must be in a voice channel!`);
   }
 });
+
 // ******************************************************************************
 
 client.login(config.token);
---
+++ commands/ping.js
@@ -0,0 +1,9 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder().setName('ping').setDescription('Show Ping'),
+
+  async execute(interaction) {
+    await interaction.reply(`>>> Ping :- \`${interaction.client.ws.ping}\``);
+  },
+};
